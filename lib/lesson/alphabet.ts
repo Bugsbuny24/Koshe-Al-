097c2e92c57db2a4b3f4a490dd6a47d8c4c98581
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-03-11 21:14:08 +0300

    Add alphabetLessons array with letters and sounds

---
+++ lib/lesson/alphabet.ts
@@ -0,0 +1,27 @@
+export const alphabetLessons = [
+  {
+    letter: "A",
+    sound: "/æ/",
+    word: "Apple"
+  },
+  {
+    letter: "B",
+    sound: "/b/",
+    word: "Ball"
+  },
+  {
+    letter: "C",
+    sound: "/k/",
+    word: "Cat"
+  },
+  {
+    letter: "D",
+    sound: "/d/",
+    word: "Dog"
+  },
+  {
+    letter: "E",
+    sound: "/e/",
+    word: "Egg"
+  }
+];
