ac60d14b47dcf19325b8aedc00046837e1b675af
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-03-15 14:34:48 +0300

    Delete app/api/auth/callback directory

--- app/api/auth/callback/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const origin = request.nextUrl.origin;
-  return NextResponse.redirect(`${origin}/`);
-}
