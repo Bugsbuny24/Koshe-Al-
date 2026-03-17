b07edfe75c67c62052916cae1dd8f3689486c5fb
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-03-17 12:27:05 +0300

    Rename createClient to createSupabaseServerClient

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,7 @@
-import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
 
-export function createClient() {
+export function createSupabaseServerClient() {
   const cookieStore = cookies();
 
   return createServerClient(
@@ -9,13 +9,13 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return cookieStore.get(name)?.value;
         },
-        set(name, value, options) {
+        set(name: string, value: string, options: any) {
           cookieStore.set({ name, value, ...options });
         },
-        remove(name, options) {
+        remove(name: string, options: any) {
           cookieStore.set({ name, value: "", ...options });
         },
       },
